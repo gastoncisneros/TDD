a549f3a591ed515b2e6efa362961ec6a510a4844
GCisneros-Everse <gcisneros@e-verse.co>
2024-06-05 20:29:54 -0300

    Add existins project

---
+++ FormulaApp.API/Controllers/FansController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormulaApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FansController : Controller
+    {
+        [HttpGet("GetFans")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("fans");
+        }
+    }
+}
